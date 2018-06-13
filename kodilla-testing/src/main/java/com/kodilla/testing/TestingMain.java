77ffa4df022349d6e02e88b86b1c0d0b99506312
KaroLLain <39537241+KaroLLain@users.noreply.github.com>
2018-06-13 21:03:15 +0200

    first commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
     public static void main (String[]args)
     {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+                if(result.equals("theForumUser")){
+                    System.out.println("test OK");
+                }else{
+                    System.out.println("Error!");
+                }
+
+        Calculator calculator = new Calculator();
+
+        int additionResult = calculator.add(10,7);
+
+                if(additionResult == additionResult){
+                    System.out.println(additionResult + " " + "Test OK");
+                }else{
+                    System.out.println("Error");
+                }
+        int subtractionResult = calculator.subtract(10,7);
+
+                if (subtractionResult == subtractionResult){
+                    System.out.println(subtractionResult + " " + "Test OK");
+                }else{
+                    System.out.println("Error");
+                }
     }
 }
