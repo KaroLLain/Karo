dc7562b4d6d8cdf9fe0a84e5dedaeb0bf60752ad
KaroLLain <39537241+KaroLLain@users.noreply.github.com>
2018-07-13 12:58:23 +0200

    BookLibrary

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -0,0 +1,31 @@
+package com.kodilla.testing.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookLibrary {
+
+    LibraryDataBase libraryDataBase;
+
+    public BookLibrary (LibraryDataBase libraryDataBase){
+
+        this.libraryDataBase = libraryDataBase;
+    }
+
+    public List<Book> listBookWithCondition(String titleFragment){
+
+            List<Book> bookList = new ArrayList<Book>();
+            if(titleFragment.length() < 3) return bookList;
+            List<Book> resultList = libraryDataBase.listBookWithCondition(titleFragment);
+            if(resultList.size() > 20) return bookList;
+            bookList = resultList;
+
+            return bookList;
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser){
+        List<Book> booksInHandsOfLibraryUser = libraryDataBase.listBooksInHandsOf(libraryUser);
+
+        return booksInHandsOfLibraryUser;
+    }
+}
