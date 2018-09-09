e1582a4e9a2639505d66f19ec266790b149c98be
KaroLLain <39537241+KaroLLain@users.noreply.github.com>
2018-09-09 12:21:33 +0200

    TaskList

--- kodilla-hibernate/src/main/java/com/kodilla/kodilla/hibernate/taskList/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/kodilla/hibernate/taskList/TaskList.java
@@ -4,7 +4,6 @@ import com.kodilla.kodilla.hibernate.task.Task;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -14,7 +13,6 @@ public class TaskList {
     private int id;
     private String listName;
     private String description;
-    private List<Task> tasks = new ArrayList<>();
 
     public TaskList(String listName, String description) {
         this.listName = listName;
@@ -42,17 +40,6 @@ public class TaskList {
         return description;
     }
 
-    @OneToMany(
-            targetEntity = Task.class,
-            mappedBy = "taskList",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY
-    )
-
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -63,9 +50,5 @@ public class TaskList {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    private void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
     }
 }
